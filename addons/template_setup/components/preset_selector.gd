6bcf552855fac595797ff1ac832cbb88752e6d35
Boop <onpotis@gmail.com>
2025-11-28 18:35:34 +1300

    Remove some old stuff + move renderer selector to own scene

--- addons/template_setup/components/preset_selector.gd
+++ addons/template_setup/components/preset_selector.gd
@@ -15,21 +15,12 @@ var is_primary: bool = false:
 			_set_primary.visible = true
 		else:
 			_set_primary.visible = false
-		print("Primary visible:")
-		print(_set_primary.visible)
 		is_primary = value
 
 var _set_primary: Button
 
 
 func _ready() -> void:
-	# Clear the children
-	for child: Node in get_children(true):
-		child.queue_free()
-	print("removed children")
-	await get_tree().create_timer(1.0).timeout
-	print("Things")
-	
 	# If there's no primary button already
 	if get_child_count(true) == 1 and _set_primary == get_child(0, true):
 		return
@@ -64,4 +55,3 @@ func _make_new_set_primary_button() -> void:
 	_set_primary.set_anchors_and_offsets_preset(Control.PRESET_RIGHT_WIDE, Control.PRESET_MODE_KEEP_SIZE)
 	#set_primary_button.visible = false
 	_set_primary.pressed.connect(_on_set_primary_pressed)
-	print(_set_primary.position,"\t",_set_primary.size)
